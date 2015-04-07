95fbd305e6806d8c8dac5fad02dadcfbe30b8913
woodbine <ian@spendnetwork.com>
2015-04-07 20:50:55 +0100

    ported from bolton test

--- scraper.py
+++ scraper.py
@@ -1,23 +1,47 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
 
-# import scraperwiki
-# import lxml.html
-#
+import scraperwiki
+import urllib2
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+# Set entity_id
+entity_id = 'E4201_BMBC_gov'
+
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+url = 'http://www.bolton.gov.uk/website/pages/Expenditurereports.aspx'
+html = urllib2.urlopen(url)
+soup = BeautifulSoup(html)
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+# # Find something on the page using css selectors
+box = soup.find('div',{'id':'pageDownloads'})
+blocks = box.findAll('li')
+
+# # Set up functions
+# # convert strings to numbers for months
+def convert_mth_strings ( mth_string ):
+	month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
+	#loop through the months in our dictionary
+	for k, v in month_numbers.items():
+		#then replace the word with the number
+		mth_string = mth_string.replace(k, v)
+	return mth_string
+	
+
+for block in blocks:
+	link = block.find('a').get('href').encode('utf8')
+	link = link.replace(' ','%20')
+	title = block.a.getText().encode('utf8').strip() #  get the text, encode and rmv whitespace
+	year = title.rsplit(' ',1)[1] #  gets the last word from the file title
+	month = title.rsplit(' ',2)[1] #  gets the penultimate word from the file title
+	
+	month_stem = month.upper()[:3]
+	month_val = convert_mth_strings(month_stem)
+	filename = entity_id + "_" + year + "_" + month_val
+	
+	todays_date = str(datetime.now())
+	
+	scraperwiki.sqlite.save(unique_keys=['l'], data={"l": link, "f": filename, "d": todays_date })
+	
+	print filename
